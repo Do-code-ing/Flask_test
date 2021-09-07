1b6ae4c4d462ed38721ef2496f320c15cd0a15a5
Do-code-ing <zxcvbn658@naver.com>
2021-09-08 05:11:41 +0900

    2021-09-08

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("Test")
 
@@ -8,4 +8,10 @@ def home():
     return render_template("potato.html")
 
 
-app.run()
+@app.route("/report")
+def report():
+    word = request.args.get("word")
+    return render_template("report.html", searching_by=word)
+
+
+app.run(debug=True)
