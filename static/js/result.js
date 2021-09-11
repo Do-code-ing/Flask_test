4226f5f502eb2426fbc4d91be653ac215f65c6e8
Do-code-ing <zxcvbn658@naver.com>
2021-09-11 15:05:11 +0900

    2021-09-11

--- static/js/result.js
+++ static/js/result.js
@@ -2,14 +2,23 @@
 
 const applyBtn = document.querySelectorAll(".result_apply_btn");
 const upBtn = document.getElementById("page_up_btn");
+const resultTbody = document.getElementById("result_tbody");
 
 function handleApplyBtnClick(event) {
   const url = event.target.attributes.url.value;
   window.open(url);
 }
 
+function handleUpBtnClick() {
+  resultTbody.scrollTo(0, 0);
+}
+
 if (applyBtn) {
   applyBtn.forEach((btn) => {
     btn.addEventListener("click", handleApplyBtnClick);
   });
+}
+
+if (upBtn) {
+  upBtn.addEventListener("click", handleUpBtnClick);
 }
