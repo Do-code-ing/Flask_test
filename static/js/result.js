4a3081fe38fd62f0cead53a6609b3d615ff9df05
Do-code-ing <zxcvbn658@naver.com>
2021-09-11 14:50:15 +0900

    2021-09-11

--- static/js/result.js
+++ static/js/result.js
@@ -1,10 +1,15 @@
 "use strict";
 
+const applyBtn = document.querySelectorAll(".result_apply_btn");
 const upBtn = document.getElementById("page_up_btn");
 
-if (upBtn) {
-  upBtn.addEventListener("click", () => {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function handleApplyBtnClick(event) {
+  const url = event.target.attributes.url.value;
+  window.open(url);
+}
+
+if (applyBtn) {
+  applyBtn.forEach((btn) => {
+    btn.addEventListener("click", handleApplyBtnClick);
   });
 }
